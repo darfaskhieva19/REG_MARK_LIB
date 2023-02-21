ccfddb984921d3b1e677e08f9364e31d3bc7e9fc
daria_faskhieva <dashaf712@mail.ru>
2023-02-21 22:13:55 +0300

    unit-тесты

--- REG_MARK_LIB/Calculation.cs
+++ REG_MARK_LIB/Calculation.cs
@@ -10,46 +10,161 @@ namespace REG_MARK_LIB
     public class Calculation
     {
         public static List<char> symbol = new List<char>() { 'A', 'B', 'E', 'K', 'M', 'H', 'O', 'P', 'C', 'T', 'Y', 'X' };
-        public static bool CheckMark(String mark) //Правильность номерного знака
+        public static bool CheckMark(String mark) 
+        {
+            if (Regex.IsMatch(mark, "^[ABEKMHOPCTYX]{1}[0-9]{3}[ABEKMHOPCTYX]{2}[0-9]{3}"))
+            {
+                return false;
+            }
+            else
+            {
+                string num = mark.Substring(1, mark.Length - 6);
+                string kodReg = mark.Substring(mark.Length - 3);
+                if (num == "000" || kodReg == "000")
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        public static string GetNextMarkAfter(String mark) 
         {
             try
             {
-                Regex regex = new Regex("^[ABEKMHOPCTYX]{1}[0-9]{3}[ABEKMHOPCTYX]{2}[0-9]{3}");
-                bool a = regex.IsMatch(mark);
-                if (a)
+                string Nmark = "";
+                string num = mark.Substring(1, mark.Length - 6);
+                if (num == "999")
                 {
-                    string registrNumber = mark.Substring(1, mark.Length - 6);
-                    string registCode = mark.Substring(mark.Length - 3);
-                    if(registCode == "000" || registrNumber == "000")
+                    num = "001";
+                    string seria = "" + mark[0] + mark[4] + mark[5];
+                    if (seria[2] != symbol[symbol.Count - 1])
                     {
-                        return false;
+                        int indexSym = -1;
+                        for (int i = 0; i < symbol.Count; i++)
+                        {
+                            if (symbol[i] == seria[2])
+                            {
+                                indexSym = i;
+                            }
+                        }
+                        Nmark = "" + seria[2] + num + seria[1] + symbol[indexSym + 1] + mark[mark.Length - 3] + mark[mark.Length - 2] + mark[mark.Length - 1];
                     }
                     else
                     {
-                        return true
+                        if (seria[0] != symbol[symbol.Count - 1])
+                        {
+                            int indexSymbol = -1;
+                            for (int i = 0; i < symbol.Count; i++)
+                            {
+                                if (symbol[i] == seria[0])
+                                {
+                                    indexSymbol = i;
+                                }
+                            }
+                            Nmark = "" + symbol[indexSymbol + 1] + num + seria[1] + symbol[0] + mark[mark.Length - 3] + mark[mark.Length - 2] + mark[mark.Length - 1];
+                        }
+                        else
+                        {
+                            if (seria[1] != symbol[symbol.Count - 1])
+                            {
+                                int indexSymbol = -1;
+                                for (int i = 0; i < symbol.Count; i++)
+                                {
+                                    if (symbol[i] == seria[1])
+                                    {
+                                        indexSymbol = i;
+                                    }
+                                }
+                                Nmark = "" + symbol[0] + num + symbol[indexSymbol + 1] + symbol[0] + mark[mark.Length - 3] + mark[mark.Length - 2] + mark[mark.Length - 1];
+                            }
+                            else
+                            {
+                                Nmark = "Ошибка";
+                            }
+                        }
                     }
                 }
                 else
                 {
-                    return false;
+                    num = string.Format("{0:D3}", Convert.ToInt32(num) + 1);
+                    int j = 0;
+                    for (int i = 0; i < mark.Length; i++)
+                    {
+                        if (i == 1 || i == 2 || i == 3)
+                        {
+                            Nmark += num[j];
+                            j++;
+                        }
+                        else
+                        {
+                            Nmark += mark[i];
+                        }
+                    }
                 }
-            }
+                return Nmark;
+            }           
             catch
             {
-                return false;
+                return "Ошибка";
             }
         }
-        public string GetNextMarkAfter(String mark) //Выдаёт следующий знак в данной серии или создает следующую серию
+        public static string GetNextMarkAfterInRange(String prevMark, String rangeStart, String rangeEnd) //Выдаёт следующий номер
         {
+            if(rangeStart == rangeEnd)
+            {
+                string result = GetNextMarkAfter(prevMark);
+                if(result == rangeStart)
+                {
+                    return result;
+                }
+                else
+                {
+                    return "out of stock";
+                }
+            }
+            else
+            {
+                List<string> list = new List<string>() { rangeStart, rangeEnd };
+                list.Sort();
 
+                if (list[0] == rangeEnd)
+                {
+                    return "out of stock";
+                }
+                else
+                {
+                    string res = rangeStart;
+                    for (int i = 0; res != rangeEnd; i++)
+                    {
+                        res = GetNextMarkAfter(res);
+
+                        if (res == prevMark && res != rangeEnd)
+                        {
+                            return GetNextMarkAfter(res);
+                        }
+                    }
+                    return "out of stock";
+                }
+            }
         }
-        public string GetNextMarkAfterInRange(String prevMark, String rangeStart, String rangeEnd) //Выдаёт следующий номер
+        public static int GetCombinationsCountInRange(String mark1, String mark2) //Расчёт оставшихся свободных номеров для региона
         {
-
-        }
-        public int GetCombinationsCountInRange(String mark1, String mark2) //Расчёт оставшихся свободных номеров для региона
-        {
-
+            string kodReg = mark1.Substring(mark1.Length - 3);
+            string KodRegion = mark1.Substring(mark1.Length - 3);
+            if(kodReg != KodRegion)
+            {
+                return 0;
+            }
+            int k = 0;
+            for(int i = 0; mark1 != mark2; i++)
+            {
+                mark1 = GetNextMarkAfter(mark1);
+                k++;
+            }
+            return k;           
         }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using REG_MARK_LIB;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckMark_correctly()
+        {
+            string mark = "Е533ЕА152";
+            bool actual = Calculation.CheckMark(mark);
+            Assert.IsTrue(actual);
+        }
+        [TestMethod]
+        public void CheckMark_NoCorrectly()
+        {
+            string mark = "У345AT000";
+            bool actual = Calculation.CheckMark(mark);
+            Assert.IsFalse(actual);
+        }
+        [TestMethod]
+        public void GetCombinationsCountInRange_NotCorrectly()
+        {
+            int exception = 150;
+            string mark1 = "A001AA152";
+            string mark2 = "A010AA152";
+            int actual = Calculation.GetCombinationsCountInRange(mark1, mark2);
+            Assert.IsTrue(exception != actual);
+        }
+        [TestMethod]
+        public void GetNextMarkAfter_NoCorrectly()
+        {
+            string expected = "A006AA170";
+            string mark = "A004AA170";
+            string actual = Calculation.GetNextMarkAfter(mark);
+            Assert.IsFalse(expected == actual);
+        }
+        [TestMethod]
+        public void GetNextMarkAfter_ResultCorrectly()
+        {
+            string exception = "A006AA152";
+            string prevMark = "A005AA152";
+            string rangeStart = "A001AA152";
+            string rangeEnd = "A800AB152";
+            string actual = Calculation.GetNextMarkAfterInRange(prevMark, rangeStart, rangeEnd);
+            Assert.IsTrue(exception == actual);
+        }
+        [TestMethod]
+        public void GetNextMarkAfter_NotNull()
+        {
+            string mark = "A004AA170";
+            string actual = Calculation.GetNextMarkAfter(mark);
+            Assert.IsNotNull(actual);
+        }
+        [TestMethod]
+        public void GetNextMarkAfterInRange_NoResult()
+        {
+            string expected = "В115ВВ116";
+            string actual = Calculation.GetNextMarkAfterInRange("ВoooВВ116", "В100ВВ116", "В130ВВ116");
+            Assert.AreNotEqual(expected, actual);
+        }
+        [TestMethod]
+        public void GetCombinationsCountInRange_Result()
+        {
+            int expected = 85;
+            int actual = Calculation.GetCombinationsCountInRange("В000ВВ152", "В085ВВ152");
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void GetCombinationsCountInRange_NoResult()
+        {
+            int expected = 95;
+            int actual = Calculation.GetCombinationsCountInRange("В000ВВ152", "В085ВВ152");
+            Assert.AreNotEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckMark_CorrectlyType()
+        {
+            string mark = "А001АА152";
+            bool actual = Calculation.CheckMark(mark);
+            Assert.IsInstanceOfType(actual, typeof(bool));
+        }
+        [TestMethod]
+        public void GetCombinationsCountInRange_Correctly()
+        {
+            int exception = 1008;
+            string mark1 = "A001AA152";
+            string mark2 = "A010AB152";
+            int actual = Calculation.GetCombinationsCountInRange(mark1, mark2);
+            Assert.AreEqual(exception, actual);
+        }
+        [TestMethod]
+        public void GetNextMarkAfterInRange_NotCorrectlyRegion()
+        {
+            string exception = "out of stock";
+            string prevMark = "A006AA777";
+            string rangeStart = "A001AA155";
+            string rangeEnd = "A800AB155";
+            string actual = Calculation.GetNextMarkAfterInRange(prevMark, rangeStart, rangeEnd);
+            Assert.AreEqual(exception, actual);
+        }
+        [TestMethod]
+        public void CheckMark_NoCorrectlyRegion()
+        {
+            bool exception = false;
+            string mark = "У345ЕT000";
+            bool actual = Calculation.CheckMark(mark);
+            Assert.AreEqual(exception, actual);
+        }
+        [TestMethod]
+        public void GetNextMarkAfterInRange_NoCorrectly()
+        {
+            string exception = "out of stock";
+            string prevMark = "A980AB777";
+            string rangeStart = "A001AA777";
+            string rangeEnd = "A800AB777";
+            string actual = Calculation.GetNextMarkAfterInRange(prevMark, rangeStart, rangeEnd);
+            Assert.IsTrue(exception == actual);
+        }
+        [TestMethod]
+        public void GetNextMarkAfterInRange_NoResultCorrectly()
+        {
+            string expected = "В110аа152";
+            string actual = Calculation.GetNextMarkAfterInRange("111111152", "В120аа152", "В130аа152");
+            Assert.AreNotEqual(expected, actual);
+        }
+
+    }
+}
